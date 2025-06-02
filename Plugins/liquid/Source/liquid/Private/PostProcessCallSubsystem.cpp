6e5b474948854c85870402b4162c7830d97e6626
TERABYTE0130 <engineerillust@gmail.com>
2025-06-02 15:34:27 +0900

    途中

--- Plugins/liquid/Source/liquid/Private/PostProcessCallSubsystem.cpp
+++ Plugins/liquid/Source/liquid/Private/PostProcessCallSubsystem.cpp
@@ -3,23 +3,137 @@
 
 #include "PostProcessCallSubsystem.h"
 
-void UPostProcessCallSubsystem::Tick(float DeltaTime)
+#include "Camera/CameraComponent.h"
+#include "DataInterface/NiagaraDataInterfaceDataChannelRead.h"
+#include "Kismet/GameplayStatics.h"
+
+static constexpr TCHAR PostProcessTableAssetPath[] = TEXT("/liquid/post_process/sample_table");
+void UPostProcessCallSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-	Super::Tick(DeltaTime);
+	Super::Initialize(Collection);
+
+	PostProcessTable = Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), nullptr, PostProcessTableAssetPath));
+	
+	if(!PostProcessTable)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[UPostProcessCallSubsystem] Data Table Load Failed"));	
+	}
 }
 
 void UPostProcessCallSubsystem::PlayPostEffect(const FName EffectID)
 {
+	if(!PostProcessTable)
+	{
+		return;
+	}
+	if(const FPostProcessConfig* Row = PostProcessTable->FindRow<FPostProcessConfig>(EffectID, TEXT("PostProcessCallSubsystem")))
+	{
+		BeginEffect(*Row);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[UPostProcessCallSubsystem] Not Found ID: %s "), *EffectID.ToString());	
+	}
 }
 
 void UPostProcessCallSubsystem::StopCurrentEffect()
 {
+	if(IsPlaying)
+	{
+		EndEffect();
+	}
 }
 
-void UPostProcessCallSubsystem::BeginPlayEffect(const FPostProcessConfig& Config)
+void UPostProcessCallSubsystem::BeginEffect(const FPostProcessConfig& Config)
 {
+	EndEffect();
+
+	CurrentSettings = Config;
+	ElapsedTime = .0f;
+	IsPlaying = true;
+
+	UMaterialInstance* Mat = Config.Material;
+	if(!Mat)
+	{
+		UE_LOG(LogTemp, Error, TEXT("[UPostProcessCallSubsystem] Material is nullptr "));
+		IsPlaying = false;
+		return;
+	}
+	CurrentPlayMID = UMaterialInstanceDynamic::Create(Mat,this);
+
+	Blendable.Object = CurrentPlayMID;
+	Blendable.Weight = 1.0f;
+	//Blendable.
+	OverrideSettings.WeightedBlendables.Array.Add(Blendable);
+	//OverrideSettings.bOverride_w
+	//OverrideSettings.bOverride_ColorSaturation = true;
+	//OverrideSettings.ColorSaturation = FVector4(0.f, 0.f, 0.f, 1.f);
 }
 
 void UPostProcessCallSubsystem::EndEffect()
 {
+	if(!IsPlaying)
+	{
+		return;
+	}
+	OverrideSettings.WeightedBlendables.Array.Reset();
+	CurrentPlayMID = nullptr;
+	IsPlaying = false;
+	auto PCM = UGameplayStatics::GetPlayerCameraManager(GetWorld(),0);
+	//PCM->ClearCa
+}
+
+void UPostProcessCallSubsystem::Tick(float DeltaTime)
+{
+	if(!IsPlaying)
+		return;
+	UWorld* CurrentWorld = GetWorld();
+	if(!CurrentWorld)
+		return;
+	
+	//UE_LOG(LogTemp, Warning, TEXT("CachedPPBlends: %d"));
+
+	ElapsedTime += DeltaTime;
+	const float Duration = FMath::Max(CurrentSettings.Duration, KINDA_SMALL_NUMBER);
+
+	float Alpha = FMath::Clamp(ElapsedTime / Duration, .0f, 1.0f);
+	float Value = .0f;
+	//if(CurrentSettings.para)
+	
+	Value = (Alpha < .5f) ? Alpha * 2.0f : (1.0 - Alpha) * 2.0f; //三角
+
+	auto PCM = UGameplayStatics::GetPlayerCameraManager(CurrentWorld,0);
+	//auto PCM = UGameplayStatics::GetPlayerCameraManager(CurrentWorld,0);
+	PCM->AddCachedPPBlend(OverrideSettings, 1.1f, VTBlendOrder_Override);
+	//APlayerCameraManager* CameraManager = PCM->PlayerCameraManager;
+	if (PCM)
+	{
+		// 現在のViewTargetを取得
+		AActor* ViewTarget = PCM->GetViewTarget();
+		if (ViewTarget)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("ViewTarget: %s"), *ViewTarget->GetName());
+
+			// カメラコンポーネントを取得して確認する
+			UCameraComponent* CameraComp = ViewTarget->FindComponentByClass<UCameraComponent>();
+			
+			if (CameraComp)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("CameraComponent is attached to ViewTarget."));
+				UE_LOG(LogTemp, Warning, TEXT("BlendWeight: %f"), CameraComp->PostProcessBlendWeight);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("No CameraComponent found in ViewTarget."));
+			}
+		}
+	}
+	
+	//PCM->
+	//if(CurrentPlayMID && CurrentSettings.ParameterName != NAME_None)
+	//{
+	//	CurrentPlayMID->SetScalarParameterValue()
+	//}
+
+	//CurrentWorld->GetWorldSettings()->SetOverridePostProcessSettings(OverrideSettings, 1.0f);
 }
--- Plugins/liquid/Source/liquid/Public/PostProcessCallSubsystem.h
+++ Plugins/liquid/Source/liquid/Public/PostProcessCallSubsystem.h
@@ -6,7 +6,6 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "Engine/EngineTypes.h"
 #include "Engine/Scene.h"
-//#include "PostProcess/confi"
 #include "PostProcessCallSubsystem.generated.h"
 
 USTRUCT(BlueprintType)
@@ -14,10 +13,10 @@ struct FPostProcessConfig : public FTableRowBase
 {
 	GENERATED_BODY()
 	
-	UPROPERTY(BlueprintReadOnly)
-	TSoftObjectPtr<UMaterialInstance> Material{nullptr};
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UMaterialInstance> Material{nullptr}; //TSoftObjectPtrの方がいいかも
 
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(EditAnywhere)
 	float Duration = 1.0f;
 };
 
@@ -29,18 +28,18 @@ class LIQUID_API UPostProcessCallSubsystem : public UTickableWorldSubsystem
 {
 	GENERATED_BODY()
 public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Tick(float DeltaTime) override;
 	virtual TStatId GetStatId() const override{RETURN_QUICK_DECLARE_CYCLE_STAT(UPostProcessCallSubsystem, STATGROUP_Tickables);}
 
 	UFUNCTION(BlueprintCallable,Category="PostProcess")
 	void PlayPostEffect(const FName EffectID);
 	void StopCurrentEffect();
-
-	UPROPERTY(EditDefaultsOnly, Category="Config")
-	TObjectPtr<UDataTable> PostProcessTable{nullptr};
+	
+	
 	
 private:
-	void BeginPlayEffect(const FPostProcessConfig& Config);
+	void BeginEffect(const FPostProcessConfig& Config);
 	void EndEffect();
 
 	float ElapsedTime = .0f;
@@ -50,7 +49,8 @@ private:
 
 	UPROPERTY()
 	TObjectPtr<UMaterialInstanceDynamic> CurrentPlayMID = nullptr;
-
+	UPROPERTY()
+	TObjectPtr<UDataTable> PostProcessTable{nullptr};
 	FPostProcessSettings OverrideSettings;
 	FWeightedBlendable Blendable{};
 	
