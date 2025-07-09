031e1ad636c7d005a9df17be92b3192daea95e6c
TERABYTE0130 <engineerillust@gmail.com>
2025-07-09 10:43:17 +0900

    ある程度動いた状態。チェックとか修正まだ

--- Plugins/liquid/Source/liquid/Private/AnimNotify_AttachNiagara.cpp
+++ Plugins/liquid/Source/liquid/Private/AnimNotify_AttachNiagara.cpp
@@ -2,46 +2,209 @@
 
 
 #include "AnimNotify_AttachNiagara.h"
-
+#include "TimerManager.h"
 #include "NiagaraComponent.h"
 #include "NiagaraFunctionLibrary.h"
 #include "NiagaraSystem.h"
+#include "Misc/UObjectToken.h"
 
 void UAnimNotify_AttachNiagara::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                        const FAnimNotifyEventReference& EventReference)
 {
 	Super::Notify(MeshComp, Animation, EventReference);
+	InitializeNiagara(MeshComp, Animation);
+	SetUpDeactivate(MeshComp, Animation);
+	SetUpDestroy(MeshComp, Animation);
 }
 
-UNiagaraComponent* UAnimNotify_AttachNiagara::SpawnNiagara(USkeletalMeshComponent* MeshComp,
-	UAnimSequenceBase* Animation)
+void UAnimNotify_AttachNiagara::BeginDestroy()
 {
-	if (!IsValid(MeshComp) || !!IsValid(Animation) || !IsValid(NiagaraSystem))
+	Super::BeginDestroy();
+}
+
+#if WITH_EDITOR
+void UAnimNotify_AttachNiagara::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+void UAnimNotify_AttachNiagara::ValidateAssociatedAssets()
+{
+	//note: UAnimNotify_PlayNiagaraEffectから引用
+	static const FName NAME_AssetCheck("AssetCheck");
+
+	if ((NiagaraSystem != nullptr) && (NiagaraSystem->IsLooping()))
+	{
+		UObject* ContainingAsset = GetContainingAsset();
+
+		FMessageLog AssetCheckLog(NAME_AssetCheck);
+
+		const FText MessageLooping = FText::Format(
+			NSLOCTEXT("AnimNotify", "NiagaraSystem_ShouldNotLoop",
+			          "Niagara system {0} used in anim notify for asset {1} is set to looping, but the slot is a one-shot (it won't be played to avoid leaking a component per notify)."),
+			FText::AsCultureInvariant(NiagaraSystem->GetPathName()),
+			FText::AsCultureInvariant(ContainingAsset->GetPathName()));
+		AssetCheckLog.Error()
+		             ->AddToken(FUObjectToken::Create(ContainingAsset))
+		             ->AddToken(FTextToken::Create(MessageLooping));
+
+		if (GIsEditor)
+		{
+			AssetCheckLog.Notify(MessageLooping, EMessageSeverity::Warning, /*bForce=*/ true);
+		}
+	}
+}
+#endif
+
+UNiagaraComponent* UAnimNotify_AttachNiagara::SpawnNiagara(USkeletalMeshComponent* MeshComp,
+                                                           const UAnimSequenceBase* Animation) const
+{
+	if (!IsValid(MeshComp))
 	{
 		return nullptr;
 	}
-	//note: 元のAPIのコメントがちょっと怪しい
+
+	if (!IsValid(Animation))
+	{
+		return nullptr;
+	}
+	if (!NiagaraSystem)
+	{
+		return nullptr;
+	}
 	if (NiagaraSystem->IsLooping())
 	{
-		return NiagaraComponent.Get();
+		if (IsValid(NiagaraComponent))
+			return NiagaraComponent.Get();
 	}
 
-	UNiagaraComponent* ReturnComp = nullptr;
-	if (AttachType == EAttachNiagaraLocationType::SocketLocation)
+	const FTransform MeshTransform = IsAttach
+		                                 ? MeshComp->GetSocketTransform(AttachSocketName, RTS_World)
+		                                 : MeshComp->GetRelativeTransform();
+	UNiagaraComponent* ReturnComp = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+		MeshComp->GetWorld(), NiagaraSystem, MeshTransform.TransformPosition(LocationOffset), FRotator::ZeroRotator,
+		FVector(1.0f), true);
+
+	if (IsAttach)
 	{
-		ReturnComp = UNiagaraFunctionLibrary::SpawnSystemAttached(NiagaraSystem, MeshComp, AttachSocketName, LocationOffset, FRotator::ZeroRotator, EAttachLocation::KeepRelativeOffset, true);
+		constexpr EAttachmentRule FixScalingRule = EAttachmentRule::KeepRelative;
+		FAttachmentTransformRules AttachRule{LocationRule, RotationRule, FixScalingRule, InWeldSimulatedBodies};
+		//ParentSocketNameがNoneまたは存在しないソケットの場合は親のTransformに直接アタッチされる
+		ReturnComp->AttachToComponent(MeshComp, AttachRule, AttachSocketName);
+	}
+	return ReturnComp;
+}
+
+void UAnimNotify_AttachNiagara::SetFloatParametersToNiagara(UNiagaraComponent* InNiagaraComponent)
+{
+	if (!IsValid(InNiagaraComponent))
+	{
+		return;
+	}
+
+	for (const auto& Param : InitialFloatParameters)
+	{
+		InNiagaraComponent->SetVariableFloat(Param.Key, Param.Value);
+	}
+}
+
+void UAnimNotify_AttachNiagara::SetSocketLocationToNiagara(UNiagaraComponent* InNiagaraComponent,
+                                                           USkeletalMeshComponent* MeshComp)
+{
+	if (!IsValid(InNiagaraComponent) || !IsValid(MeshComp))
+	{
+		return;
+	}
+	for (const auto& Param : InitialSocketLocationParameters)
+	{
+		const FTransform SocketTransform = MeshComp->GetSocketTransform(Param.Key);
+		InNiagaraComponent->SetVariablePosition(Param.Value, SocketTransform.GetLocation());
+	}
+}
+
+void UAnimNotify_AttachNiagara::InitializeNiagara(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation)
+{
+	if (DelayActivateTime > .0f)
+	{
+		FWeakObjectPtr WeakMeshComp = MeshComp;
+		FWeakObjectPtr WeakAnimation = Animation;
+		TWeakObjectPtr<UAnimNotify_AttachNiagara> WeakThis = this;
+		if (auto World = MeshComp->GetWorld())
+		{
+			FTimerHandle DelayActivateHandle;
+			World->GetTimerManager().SetTimer(DelayActivateHandle, [WeakMeshComp, WeakAnimation, WeakThis]()
+			{
+				if (auto* ThisPtr = WeakThis.Get())
+				{
+					if (auto* MeshCompPtr = Cast<USkeletalMeshComponent>(WeakMeshComp.Get()))
+					{
+						if (auto* AnimationPtr = Cast<UAnimSequenceBase>(WeakAnimation.Get()))
+						{
+							ThisPtr->ActivateNiagara(MeshCompPtr, AnimationPtr);
+						}
+					}
+				}
+			}, DelayActivateTime, false);
+		}
 	}
 	else
 	{
-		const FTransform MeshTransform = MeshComp->GetRelativeTransform();
-		ReturnComp = UNiagaraFunctionLibrary::SpawnSystemAtLocation(MeshComp->GetWorld(), NiagaraSystem, MeshTransform.TransformPosition(LocationOffset), FRotator::ZeroRotator, FVector(1.0f),true);
+		ActivateNiagara(MeshComp, Animation);
+	}
+}
+
+void UAnimNotify_AttachNiagara::ActivateNiagara(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation)
+{
+	NiagaraComponent = SpawnNiagara(MeshComp, Animation);
+	SetFloatParametersToNiagara(NiagaraComponent);
+	SetSocketLocationToNiagara(NiagaraComponent, MeshComp);
+}
+
+void UAnimNotify_AttachNiagara::SetUpDeactivate(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation)
+{
+	if (!IsValid(MeshComp) || !IsValid(NiagaraComponent))
+	{
+		return;
 	}
 
-	if (ReturnComp != nullptr)
+	if (DeactiveTime > .0f)
 	{
-		//ReturnComp->SetUsingAbsoluteScale(bAhbsoluteScale);
-		//ReturnComp->SetRelativeScale3D_Direct(Scale);
+		TWeakObjectPtr<UNiagaraComponent> WeakNiagaraComponent = NiagaraComponent;
+		if (auto World = MeshComp->GetWorld())
+		{
+			FTimerHandle DeactivateHandle;
+			World->GetTimerManager().SetTimer(DeactivateHandle, [WeakNiagaraComponent]()
+			{
+				if (auto* NiagaraComponentPtr = WeakNiagaraComponent.Get())
+				{
+					NiagaraComponentPtr->Deactivate();
+				}
+			}, DeactiveTime, false);
+		}
 	}
-	//ReturnComp->AttachToComponent()
-	return ReturnComp;
+}
+
+void UAnimNotify_AttachNiagara::SetUpDestroy(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation)
+{
+	if (!IsValid(MeshComp) || !IsValid(NiagaraComponent))
+	{
+		return;
+	}
+
+	if (DestroyTime > .0f)
+	{
+		TWeakObjectPtr<UNiagaraComponent> WeakNiagaraComponent = NiagaraComponent;
+		if (auto World = MeshComp->GetWorld())
+		{
+			FTimerHandle DestroyHandle;
+			World->GetTimerManager().SetTimer(DestroyHandle, [WeakNiagaraComponent]()
+			{
+				if (auto* NiagaraComponentPtr = WeakNiagaraComponent.Get())
+				{
+					NiagaraComponentPtr->Deactivate();
+					NiagaraComponentPtr->DestroyComponent();
+				}
+			}, DestroyTime, false);
+		}
+	}
 }
--- Plugins/liquid/Source/liquid/Public/AnimNotify_AttachNiagara.h
+++ Plugins/liquid/Source/liquid/Public/AnimNotify_AttachNiagara.h
@@ -10,15 +10,14 @@
 class UNiagaraSystem;
 class UNiagaraComponent;
 
-UENUM(BlueprintType)
-enum class EAttachNiagaraLocationType: uint8
-{
-	MeshLocation,
-	SocketLocation,
-};
-
 /**
- * 
+ * @brief Animation notify that spawns and attaches a Niagara particle system to a skeletal mesh during an animation sequence.
+ *
+ * This class facilitates the creation and management of a Niagara particle system during animation playback. It allows for
+ * configuring attachment parameters such as location, rotation, and scale rules, and supports delay activation, destruction timing,
+ * and initial particle system parameters.
+ *
+ * @note This class assumes the use of the Niagara particle system framework in Unreal Engine.
  */
 UCLASS()
 class LIQUID_API UAnimNotify_AttachNiagara : public UAnimNotify
@@ -30,34 +29,56 @@ public:
 	virtual ~UAnimNotify_AttachNiagara()override = default;
 	
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	virtual void BeginDestroy() override;
+	
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void ValidateAssociatedAssets() override;
+#endif
+	
+private:
+	UNiagaraComponent* SpawnNiagara(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation) const;
+	void SetFloatParametersToNiagara(UNiagaraComponent* InNiagaraComponent);
+	void SetSocketLocationToNiagara(UNiagaraComponent* InNiagaraComponent, USkeletalMeshComponent* MeshComp);
 
+	void InitializeNiagara(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation);
+	void ActivateNiagara(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation);
+	void SetUpDeactivate(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation);
+	void SetUpDestroy(USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation);
+
+	
 private:
-	UNiagaraComponent* SpawnNiagara(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation);
-private:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Asset", meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<UNiagaraSystem> NiagaraSystem{};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	bool IsAttach{false};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	FName AttachSocketName{};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	EAttachmentRule LocationRule{EAttachmentRule::KeepRelative};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	EAttachmentRule RotationRule{EAttachmentRule::KeepRelative};
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	//EAttachmentRule ScaleRule{EAttachmentRule::KeepRelative};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	bool InWeldSimulatedBodies{false};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Transforms", meta = (AllowPrivateAccess = "true"))
+	FVector LocationOffset;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "User Parameters", meta = (AllowPrivateAccess = "true"))
+	TMap<FName, float> InitialFloatParameters{};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "User Parameters", meta = (AllowPrivateAccess = "true"))
+	TMap<FName, FName> InitialSocketLocationParameters{};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Time Parameters", meta = (AllowPrivateAccess = "true"))
+	float DelayActivateTime{.0f};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Time Parameters", meta = (AllowPrivateAccess = "true"))
+	float DeactiveTime{-1.0};
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Time Parameters", meta = (AllowPrivateAccess = "true"))
+	float DestroyTime{2.0f};
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
+	UPROPERTY()
 	TObjectPtr<UNiagaraComponent> NiagaraComponent{}; //note: 変数として保持しなくていいかもしれない
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	EAttachNiagaraLocationType AttachType{EAttachNiagaraLocationType::SocketLocation};
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	FName AttachSocketName{};
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	FVector LocationOffset;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	TMap<FName, float> InitialFloatParameters{};
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	float DelayActivateTime{.0f};
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	float DestroyTime{1.0f};
-	
-	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Parameters", meta = (AllowPrivateAccess = "true"))
-	//TMap<FName, FVector4> InitialVectorParameters{};
-	
+	//FTimerHandle DelayActivateHandle;
+	//FTimerHandle DeactivateHandle;
+	//FTimerHandle DestroyHandle;
 };
