0ec6df20ec943a410fdda88702619bfc6a891a41
TERABYTE0130 <engineerillust@gmail.com>
2026-01-26 01:07:01 +0900

    init module

---
+++ Plugins/liquid/Source/liquid_editor/Public/NiagaraModuleSnapShot.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetRegistry/AssetData.h"
+#include "AssetActionUtility.h"
+#include "NiagaraModuleSnapShot.generated.h"
+
+UCLASS()
+class UNiagaraModuleSnapshotAction : public UAssetActionUtility
+{
+	GENERATED_BODY()
+
+public:
+	//virtual bool CanExecuteOnAssets(const TArray<FAssetData>& InAssets) const override;
+
+	
+	UFUNCTION(CallInEditor, Category = "Niagara")
+	void CreateNiagaraModuleSnapshot();
+};
